8446fda796e88316b69cca8c079865554e8ebac3
Manuel A. Mena S <trots.developer@protonmail.com>
2019-09-18 23:36:11 +0000

    New commit

--- rest/rest.go
+++ rest/rest.go
@@ -7,7 +7,7 @@ import (
 )
 
 type RegisteredUsers []dao.RegisteredUser
-type PaymentMethodType []dao.PaymentMethodType
+type PaymentMethodTypes []dao.PaymentMethodType
 type PaymentMethods []dao.PaymentMethod
 type Purchases []dao.Purchase
 type ProductCategories []dao.ProductCategory
@@ -26,11 +26,19 @@ func GetAllUsers(w http.ResponseWriter, r *http.Request) {
     var err error
     users, err = user.SelectAllInDB()
     response := Response{}
+    respB, err2 := json.Marshal(users)
+    
     if err != nil {
+        response.Code = "404"
+        response.Content = err              
+    } else if err2 != nil {
+        response.Code = "404"
+        response.Content = err2
+    } else {
         response.Code = "200"
-        response.Content = users       
+        response.Content = users
     }
-    respB, _ := json.Marshal(users)
+    
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
@@ -44,42 +52,211 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {}
 func DeleteUser(w http.ResponseWriter, r *http.Request) {}
 
 // payment_method_types
-func GetAllPaymentMethodTypes(w http.ResponseWriter, r *http.Request) {}
+func GetAllPaymentMethodTypes(w http.ResponseWriter, r *http.Request) {
+    paymentMethodType := dao.PaymentMethodType{}
+    var paymentMethodTypes PaymentMethodTypes
+    var err error
+    paymentMethodTypes, err = paymentMethodType.SelectAllInDB()
+    response := Response{}
+    respB, err2 := json.Marshal(paymentMethodTypes)
+    
+    if err != nil {
+        response.Code = "404"
+        response.Content = err              
+    } else if err2 != nil {
+        response.Code = "404"
+        response.Content = err2
+    } else {
+        response.Code = "200"
+        response.Content = paymentMethodTypes
+    }
+    
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write(respB[:])   
+    
+}
 func GetPaymentMethodType(w http.ResponseWriter, r *http.Request) {}
 
 // payment_methods
-func GetAllPaymentMethods(w http.ResponseWriter, r *http.Request) {}
+func GetAllPaymentMethods(w http.ResponseWriter, r *http.Request) {
+    paymentMethod := dao.PaymentMethod{}
+    var paymentMethods PaymentMethods
+    var err error
+    paymentMethods, err = paymentMethod.SelectAllInDB()
+    response := Response{}   
+    respB, err2 := json.Marshal(paymentMethods)
+    
+    if err != nil {
+        response.Code = "404"
+        response.Content = err              
+    } else if err2 != nil {
+        response.Code = "404"
+        response.Content = err2
+    } else {
+        response.Code = "200"
+        response.Content = paymentMethods
+    }
+    
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write(respB[:])   
+}
 func GetPaymentMethod(w http.ResponseWriter, r *http.Request) {}
 func CreatePaymentMethod(w http.ResponseWriter, r *http.Request) {}
 func UpdatePaymentMethod(w http.ResponseWriter, r *http.Request) {}
 func DeletePaymentMethod(w http.ResponseWriter, r *http.Request) {}
 
 // purchases
-func GetAllPurchases(w http.ResponseWriter, r *http.Request) {}
+func GetAllPurchases(w http.ResponseWriter, r *http.Request) {
+    purchase := dao.Purchase{}
+    var purchases Purchases
+    var err error
+    purchases, err = purchase.SelectAllInDB()
+    response := Response{}
+    respB, err2 := json.Marshal(purchases)
+    
+    if err != nil {
+        response.Code = "404"
+        response.Content = err              
+    } else if err2 != nil {
+        response.Code = "404"
+        response.Content = err2
+    } else {
+        response.Code = "200"
+        response.Content = purchases
+    }
+    
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write(respB[:])   
+}
 func GetPurchase(w http.ResponseWriter, r *http.Request) {}
 func CreatePurchase(w http.ResponseWriter, r *http.Request) {}
 func UpdatePurchase(w http.ResponseWriter, r *http.Request) {}
 func DeletePurchase(w http.ResponseWriter, r *http.Request) {}
 
 // product_categories 
-func GetAllProductCategories(w http.ResponseWriter, r *http.Request) {}
+func GetAllProductCategories(w http.ResponseWriter, r *http.Request) {
+    productCategory := dao.ProductCategory{}
+    var productCategories ProductCategories
+    var err error
+    productCategories, err = productCategory.SelectAllInDB()
+    response := Response{}    
+    respB, err2 := json.Marshal(productCategories)
+    
+    if err != nil {
+        response.Code = "404"
+        response.Content = err              
+    } else if err2 != nil {
+        response.Code = "404"
+        response.Content = err2
+    } else {
+        response.Code = "200"
+        response.Content = productCategories
+    }
+    
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write(respB[:])
+}
 func GetProductCategory(w http.ResponseWriter, r *http.Request) {}
 
 // product_offers
-func GetAllProductOffers(w http.ResponseWriter, r *http.Request) {}
+func GetAllProductOffers(w http.ResponseWriter, r *http.Request) {
+    productOffer := dao.ProductOffer{}
+    var products ProductOffers
+    var err error
+    products, err = productOffer.SelectAllInDB()
+    response := Response{}    
+    respB, err2 := json.Marshal(products)
+    
+    if err != nil {
+        response.Code = "404"
+        response.Content = err              
+    } else if err2 != nil {
+        response.Code = "404"
+        response.Content = err2
+    } else {
+        response.Code = "200"
+        response.Content = products
+    }
+    
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write(respB[:])
+}
 func GetProductOffer(w http.ResponseWriter, r *http.Request) {}
 func CreateProductOffer(w http.ResponseWriter, r *http.Request) {}
 func UpdateProductOffer(w http.ResponseWriter, r *http.Request) {}
 func DeleteProductOffer(w http.ResponseWriter, r *http.Request) {}
 
 // product_offer_img_urls
-func GetAllProductOfferImgUrls(w http.ResponseWriter, r *http.Request) {}
+func GetAllProductOfferImgUrls(w http.ResponseWriter, r *http.Request) {
+    productOfferImgUrl := dao.ProductOfferImgUrl{}
+    var imgUrls ProductOfferImgUrls
+    var err error
+    imgUrls, err = productOfferImgUrl.SelectAllInDB()
+    response := Response{}    
+    respB, err2 := json.Marshal(imgUrls)
+    
+    if err != nil {
+        response.Code = "404"
+        response.Content = err              
+    } else if err2 != nil {
+        response.Code = "404"
+        response.Content = err2
+    } else {
+        response.Code = "200"
+        response.Content = imgUrls
+    }
+    
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write(respB[:])   
+    
+}
 func CreateProductOfferImgUrl(w http.ResponseWriter, r *http.Request) {}
 func UpdateProductOfferImgUrl(w http.ResponseWriter, r *http.Request) {}
 func DeleteProductOfferImgUrl(w http.ResponseWriter, r *http.Request) {}
 
 // purchase_details
-func GetAllPurchaseDetails(w http.ResponseWriter, r *http.Request) {}
+func GetAllPurchaseDetails(w http.ResponseWriter, r *http.Request) {
+    purchaseDetail := dao.PurchaseDetail{}
+    var purchaseDetails PurchaseDetails
+    var err error
+    purchaseDetails, err = purchaseDetail.SelectAllInDB()    
+    response := Response{}    
+    respB, err2 := json.Marshal(purchaseDetails)
+    
+    if err != nil {
+        response.Code = "404"
+        response.Content = err              
+    } else if err2 != nil {
+        response.Code = "404"
+        response.Content = err2
+    } else {
+        response.Code = "200"
+        response.Content = purchaseDetails
+    }
+    w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write(respB[:])
+}
 func GetPurchaseDetail(w http.ResponseWriter, r *http.Request) {}
 func CreatePurchaseDetail(w http.ResponseWriter, r *http.Request) {}
 func UpdatePurchaseDetail(w http.ResponseWriter, r *http.Request) {}
